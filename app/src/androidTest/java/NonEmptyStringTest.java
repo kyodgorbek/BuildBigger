fb500b9d674af65bfa748395f8c1a7144d65900a
kyodgorbek <kyodgorbek@gmail.com>
2018-01-08 16:02:10 +0100

    I have updated and added testing framework

--- app/src/androidTest/java/NonEmptyStringTest.java
+++ app/src/androidTest/java/NonEmptyStringTest.java
@@ -1,12 +1,10 @@
 import android.test.AndroidTestCase;
 import android.util.Log;
 
-
-
 /**
  * Created by yodgorbekkomilov on 1/8/18.
  */
- 
+
 public class NonEmptyStringTest extends AndroidTestCase {
 
     private static final String LOG_TAG = "NonEmptyStringTest";
