25a08e6d42d1e2086c7dbf5d719ecec2f72567ea
kyodgorbek <kyodgorbek@gmail.com>
2018-01-09 15:40:44 +0100

    I have updated and added testing framework

--- app/src/main/java/com/udacity/gradle/builditbigger/EndpointsAsyncTask.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/EndpointsAsyncTask.java
@@ -70,10 +70,7 @@ public void initialize(AbstractGoogleClientRequest<?> abstractGoogleClientReques
 
         @Override
         protected void onPostExecute(String result) {
-                Intent intent = new Intent(context, JokeActivity.class);
-                intent.setAction(Intent.ACTION_SEND);
-                intent.putExtra( MainActivity.JOKE_KEY, result);
-                context.startActivity(intent);
+
         }
 }
 
