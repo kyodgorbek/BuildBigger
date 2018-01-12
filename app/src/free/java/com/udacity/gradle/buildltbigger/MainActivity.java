70deaa4f757ceab4c707b9769c401f721ecbc536
kyodgorbek <kyodgorbek@gmail.com>
2018-01-12 04:52:29 +0100

    I have declared flavors

--- app/src/free/java/com/udacity/gradle/buildltbigger/MainActivity.java
+++ app/src/free/java/com/udacity/gradle/buildltbigger/MainActivity.java
@@ -4,5 +4,18 @@ package com.udacity.gradle.buildltbigger;
  * Created by yodgorbekkomilov on 1/12/18.
  */
 
-public class MainActivity {
+public class MainActivity extends AppCompatActivity {
+
+    public void tellJoke(View view) {
+
+        Context context = this;
+        CharSequence text = this.getString(R.string.toast_text);
+        int duration = Toast.LENGTH_LONG;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+
+
+    }
+
 }
--- app/src/paid/java/com/udacity/gradle/builditbigger/PaidActivity.java
+++ app/src/paid/java/com/udacity/gradle/builditbigger/PaidActivity.java
@@ -1,10 +1,21 @@
 package com.udacity.gradle.builditbigger;
 
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import com.example.myapplication.*;
+
 /**
  * Created by yodgorbekkomilov on 1/12/18.
  */
 
-public class PaidActivity {
+public class PaidActivity extends AppCompatActivity {
 
+
+
+    public void tellJoke(View view) {
+        Intent jokeIntent = new Intent(this, JokeActivity.class);
+        startActivity(jokeIntent);
+    }
 
 }
